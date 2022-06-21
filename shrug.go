385a4b2e8621c6379cda39e9f4b14c579f0e06b3
promiselb <107135539+promiselb@users.noreply.github.com>
2022-06-22 01:01:23 +0300

    Create shrug.go

---
+++ shrug.go
@@ -0,0 +1,30 @@
+package discordgo
+
+import (
+	"strings"
+
+	GG "github.com/bwmarrin/discordgo"
+)
+
+func ShrugfHandler(s *GG.Session, m *GG.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.Contains(m.Content, Shrugcmd.name) {
+		shrugProcess(s, m)
+		return
+	}
+}
+
+func shrugProcess(s *GG.Session, m *GG.MessageCreate) {
+	var ss string
+
+	for i := 0; i < len(m.Content); i++ {
+	slice := strings.Split(m.Content, Shrugcmd.name)
+	if len(slice) <= 1 {
+		return
+	}
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintln(slice[0]+slice[1], Shrugcmd.value))
+	Shrugcmd.Log("")
+}
